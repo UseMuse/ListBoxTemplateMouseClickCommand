493e3a66316bb037cbecbc566a28f94780d96019
UseMuse <UseMuse@mail.ru>
2021-09-16 18:03:47 +0300

    Добавление асинхронного RelayCommand, брал отсюда https://docs.microsoft.com/ru-ru/archive/msdn-magazine/2014/april/async-programming-patterns-for-asynchronous-mvvm-applications-commands

---
+++ Common/Simplified/RelayCommandAsync.cs
@@ -0,0 +1,132 @@
+﻿using Simplified;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Common.Simplified
+{
+    public class RelayCommandAsync<TResult> : RelayCommandAsyncBase, INotifyPropertyChanged
+    {
+        private readonly Func<CancellationToken, Task<TResult>> _command;
+        private readonly CancelAsyncCommand _cancelCommand;
+        private NotifyTaskCompletion<TResult> _execution;
+
+        public RelayCommandAsync(Func<CancellationToken, Task<TResult>> command)
+        {
+            _command = command;
+            _cancelCommand = new CancelAsyncCommand();
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return Execution == null || Execution.IsCompleted;
+        }
+
+        public override async Task ExecuteAsync(object parameter)
+        {
+            _cancelCommand.NotifyCommandStarting();
+            Execution = new NotifyTaskCompletion<TResult>(_command(_cancelCommand.Token));
+            RaiseCanExecuteChanged();
+            await Execution.TaskCompletion;
+            _cancelCommand.NotifyCommandFinished();
+            RaiseCanExecuteChanged();
+        }
+
+        public ICommand CancelCommand
+        {
+            get { return _cancelCommand; }
+        }
+
+        public NotifyTaskCompletion<TResult> Execution
+        {
+            get { return _execution; }
+            private set
+            {
+                _execution = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private sealed class CancelAsyncCommand : ICommand
+        {
+            private CancellationTokenSource _cts = new CancellationTokenSource();
+            private bool _commandExecuting;
+
+            public CancellationToken Token { get { return _cts.Token; } }
+
+            public void NotifyCommandStarting()
+            {
+                _commandExecuting = true;
+                if (!_cts.IsCancellationRequested)
+                    return;
+                _cts = new CancellationTokenSource();
+                RaiseCanExecuteChanged();
+            }
+
+            public void NotifyCommandFinished()
+            {
+                _commandExecuting = false;
+                RaiseCanExecuteChanged();
+            }
+
+            bool ICommand.CanExecute(object parameter)
+            {
+                return _commandExecuting && !_cts.IsCancellationRequested;
+            }
+
+            void ICommand.Execute(object parameter)
+            {
+                _cts.Cancel();
+                RaiseCanExecuteChanged();
+            }
+
+            public event EventHandler CanExecuteChanged
+            {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+            }
+
+            private void RaiseCanExecuteChanged()
+            {
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
+    }
+
+    public static class RelayCommandAsync
+    {
+        public static RelayCommandAsync<object> Create(Func<Task> command)
+        {
+            return new RelayCommandAsync<object>(async _ => { await command(); return null; });
+        }
+
+        public static RelayCommandAsync<TResult> Create<TResult>(Func<Task<TResult>> command)
+        {
+            return new RelayCommandAsync<TResult>(_ => command());
+        }
+
+        public static RelayCommandAsync<object> Create(Func<CancellationToken, Task> command)
+        {
+            return new RelayCommandAsync<object>(async token => { await command(token); return null; });
+        }
+
+        public static RelayCommandAsync<TResult> Create<TResult>(Func<CancellationToken, Task<TResult>> command)
+        {
+            return new RelayCommandAsync<TResult>(command);
+        }
+    }
+}
---
+++ Common/Simplified/RelayCommandAsyncBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Common.Simplified
+{
+    public abstract class RelayCommandAsyncBase : IAsyncCommand
+    {
+        public abstract bool CanExecute(object parameter);
+
+        public abstract Task ExecuteAsync(object parameter);
+
+        public async void Execute(object parameter)
+        {
+            await ExecuteAsync(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        protected void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+    }
+}
